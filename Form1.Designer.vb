203e9b407a77c6cdf1105a790a57dee88c07926e
xela <thegamerale97@gmail.com>
2022-05-20 15:28:22 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_spesaTotale
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbl_totSpesa = New System.Windows.Forms.Label()
+        Me.btn_esci = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lbl_totSpesa
+        '
+        Me.lbl_totSpesa.AutoSize = True
+        Me.lbl_totSpesa.Location = New System.Drawing.Point(87, 150)
+        Me.lbl_totSpesa.Name = "lbl_totSpesa"
+        Me.lbl_totSpesa.Size = New System.Drawing.Size(0, 15)
+        Me.lbl_totSpesa.TabIndex = 0
+        '
+        'btn_esci
+        '
+        Me.btn_esci.Location = New System.Drawing.Point(359, 311)
+        Me.btn_esci.Name = "btn_esci"
+        Me.btn_esci.Size = New System.Drawing.Size(75, 23)
+        Me.btn_esci.TabIndex = 1
+        Me.btn_esci.Text = "Esci"
+        Me.btn_esci.UseVisualStyleBackColor = True
+        '
+        'frm_spesaTotale
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(446, 346)
+        Me.Controls.Add(Me.btn_esci)
+        Me.Controls.Add(Me.lbl_totSpesa)
+        Me.Name = "frm_spesaTotale"
+        Me.Text = "Spesa totale"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbl_totSpesa As Label
+    Friend WithEvents btn_esci As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class frm_spesaTotale
+    Private Sub frm_spesaTotale_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim numProd, cont As Byte
+        Dim prezzo, tot As Single
+        numProd = Val(InputBox("Quanti prodotti sono stato acquistati?", "input"))
+        tot = 0
+        cont = 1
+        Do While cont <= numProd
+            prezzo = Val(InputBox("inserisci il prezzo", CStr(cont) & "° prodotto"))
+            tot = tot + prezzo
+            cont = cont + 1
+        Loop
+        lbl_totSpesa.Text = "Importo totale della spesa: " & CStr(tot)
+    End Sub
+
+    Private Sub btn_esci_Click(sender As Object, e As EventArgs) Handles btn_esci.Click
+        End
+    End Sub
+End Class
